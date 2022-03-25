3e3ba72b4456947d9f789a8d6920f6b89bb10755
sayman <mdrabbi.asm@gmail.com>
2022-03-25 21:23:45 +0600

    make heade

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Component/Header/Header';
 function App() {
   return (
     <div>
-      
+      <Header></Header>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Header.css'
+
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='header'>
+            <Navbar expand="lg">
+  <Container>
+    <Navbar.Brand href="#home" className='text-white fs-bold fs-3'>BIKE <span className='brand-title'>DB</span></Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ms-auto">
+        <Nav.Link href="#home" className='text-white fs-5 me-3'>Home</Nav.Link>
+        <Nav.Link href="#link"className='text-white fs-5 me-3'>Shop</Nav.Link>
+        <Nav.Link href="#link" className='text-white fs-5 me-3'>Cart</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+        </nav>
     );
 };
 
