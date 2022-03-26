7a73f18f88033cc09886cda303e5b88a2b53e5cf
sayman <mdrabbi.asm@gmail.com>
2022-03-26 12:30:24 +0600

    update readme file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Component/Header/Header';
 import Shop from './Component/Header/Shop/Shop';
+import Theory from './Theory/Theory';
 function App() {
   return (
     <div>
       <Header></Header>
       <Shop></Shop>
+      <Theory></Theory>
     </div>
   );
 }
